82e82f3e5894e6bbc2c3d706a76ee50a1ed913cd
Nazeef Ahmed <nazeefahmed17@gmail.com>
2025-08-14 08:17:48 +0530

    Fix build script for Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,9 @@
-set -e
-
 # Install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# (Optional) Run migrations if needed
+python manage.py migrate
